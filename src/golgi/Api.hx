3960919bc34a94a8a1d4fe47715d2bb56b0c4ee4
Justin Donaldson <jdonaldson@gmail.com>
2018-01-02 21:21:21 -0800

    use a faster internal datastructure for lua

--- src/golgi/Api.hx
+++ src/golgi/Api.hx
@@ -1,6 +1,8 @@
 package golgi;
 import golgi.meta.MetaGolgi;
 
+typedef TMap<TReq,TRet> = #if lua LuaMap<TReq,TRet> #else Map<TReq,TRet> #end
+
 /**
   The base subclass for a Golgi Api.  Classes inheriting this class will get a
   custom __golgi__ method constructed that contains routing information specific
@@ -13,7 +15,7 @@ class Api<TReq,TRet,TMeta:MetaGolgi<TReq,TRet>> {
     var __golgi_meta__ : TMeta;
 
     // path dictionary
-    var __golgi_dict__ : Map<String, Array<String>->Dynamic->Dynamic->TRet>;
+    var __golgi_dict__ : TMap<String, Array<String>->Dynamic->Dynamic->TRet>;
 
     /**
       An initializer that is replaced by the macro @:autoBuild function.
@@ -22,7 +24,7 @@ class Api<TReq,TRet,TMeta:MetaGolgi<TReq,TRet>> {
 
     public function new(meta : TMeta){
         __golgi_meta__ = meta;
-        __golgi_dict__ = new Map();
+        __golgi_dict__ = new TMap();
         __golgi_init__();
     }
 
@@ -35,3 +37,20 @@ class Api<TReq,TRet,TMeta:MetaGolgi<TReq,TRet>> {
     }
 }
 
+#if lua
+class LuaMap<K,V> {
+    var t : lua.Table<K,V>;
+    public function new(){
+        t = lua.Table.create();
+    }
+    inline public function get(k:K) : V {
+        return t[cast k];
+    }
+    inline public function set(k:K, v:V) : Void {
+        t[cast k] = v;
+    }
+    inline public function exists(k:K) : Bool {
+        return t[cast k] != null;
+    }
+}
+#end

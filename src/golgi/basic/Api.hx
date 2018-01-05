25b46033785261e71058687cce4ede77b4931723
Justin Donaldson <jdonaldson@gmail.com>
2018-01-04 18:19:05 -0800

    use explicit type path for api

--- src/golgi/basic/Api.hx
+++ src/golgi/basic/Api.hx
@@ -5,7 +5,7 @@ import golgi.meta.MetaGolgi;
   A basic version of the Golgi Api.  It does not accept a MetaGolgi argument.
  **/
 @:allow(golgi.Subroute, golgi.Golgi)
-class Api<TReq,TRet> extends Api<TReq,TRet,MetaGolgi<TReq,TRet>>{
+class Api<TReq,TRet> extends golgi.Api<TReq,TRet,MetaGolgi<TReq,TRet>>{
     public function new(){
         super(new MetaGolgi<TReq,TRet>());
     }

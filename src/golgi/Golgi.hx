8c58af26479899cad75fecb5a19c23120821285e
Justin Donaldson <jdonaldson@gmail.com>
2018-01-10 16:13:58 -0800

    better fix for cs

--- src/golgi/Golgi.hx
+++ src/golgi/Golgi.hx
@@ -6,27 +6,39 @@ import golgi.meta.MetaGolgi;
   method.
  **/
 class Golgi{
+
     public static function run<TReq, TRet>(path : Path, params : Any, request : TReq, api : Api<TReq,TRet,Dynamic>)  : TRet {
 #if cs
-        try {
+        return _runcs(path, params, request, api);
+#else
+        return _run(path, params, request, api);
 #end
+    }
+
+    inline public static function _run<TReq, TRet>(path : Path, params : Any, request : TReq, api : Api<TReq,TRet,Dynamic>)  : TRet {
         return api.__golgi__(path, params, request);
+    }
 
 #if cs
+    inline public static function _runcs<TReq, TRet>(path : Path, params : Any, request : TReq, api : Api<TReq,TRet,Dynamic>)  : TRet {
+        try{
+            return api.__golgi__(path, params, request);
         } catch (e : Dynamic) {
             if (Std.is(e, golgi.Error)){
                 throw e;
             } else {
                 var gbe = e.GetBaseException();
-                if (Std.is(gbe, golgi.Error)){
+                if (Std.is(gbe.obj, golgi.Error)){
                     throw gbe;
                 } else {
                     throw e;
                 }
             }
+            return null;
         }
-#end
     }
+#end
+
 
 }
 
